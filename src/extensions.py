5476c8a24513667d0c6ef3813c738fe2fe13adb6
potoole-afs <69931928+potoole-afs@users.noreply.github.com>
2020-08-20 14:07:15 -0400

    Update extensions.py

--- src/extensions.py
+++ src/extensions.py
@@ -1,6 +1,10 @@
 from celery import Celery
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
 
+class SQLAlchemy(_BaseSQLAlchemy):
+    def apply_pool_defaults(self, app, options):
+        super(SQLAlchemy, self).apply_pool_defaults(self, app, options)
+        options["pool_pre_ping"] = True
 db = SQLAlchemy()
 celery = Celery()
 
